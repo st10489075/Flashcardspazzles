274dda97ade9d431a46c870f915cef72ad5af55d
st10489075 <ST10489075@rcconnect.edu.za>
2025-05-15 12:21:05 +0000

    i created a flashcard app that has historical questions and also provides answers and the score for every correct answer you get a score

--- app/src/main/java/com/example/flashcardspazzles/Flashcards.kt
+++ app/src/main/java/com/example/flashcardspazzles/Flashcards.kt
@@ -48,7 +48,6 @@ class Flashcards : ComponentActivity() {
                 mutableStateOf ( "")
             }
 
-           
 
 
 
--- app/src/main/java/com/example/flashcardspazzles/ScoreDisplay.kt
+++ app/src/main/java/com/example/flashcardspazzles/ScoreDisplay.kt
@@ -1,15 +1,25 @@
 package com.example.flashcardspazzles
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.flashcardspazzles.ui.theme.FlashcardsPazzlesTheme
 
 class ScoreDisplay : ComponentActivity() {
@@ -20,11 +30,45 @@ class ScoreDisplay : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
+            var feedback by remember{
+                mutableStateOf("")
+
+            }
+
             Text(text = "Score: "+ score)
 
             if( score == 0 ){
-
+                feedback = "You did not try at all"
             }
+            if (score <3 ) {
+                feedback = "You tried"
+            }else{
+                feedback = "Job well done"
+            }
+            if (score >=6 ){
+                feedback= "Invalid score"
+            }
+
+            Text(text = feedback )
+
+            Spacer(modifier = Modifier.size(30.dp))
+
+            Text(text = "Press the button below to review work")
+
+            Spacer(modifier = Modifier.size(30.dp))
+
+            Button(
+                onClick = {
+
+                    val next = Intent(this@ScoreDisplay, ReviewThePuzzels::class.java)
+                    startActivity(next)
+
+             })
+
+            {
+                Text(text = "Review your work")
+            }
+
 
 
         }
